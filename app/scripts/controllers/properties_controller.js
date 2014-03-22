cb7a96080dc1727425094ccbb9ae09f77761cc9c
JMaxwell <maxwell.joshua@gmail.com>
2014-03-22 01:37:26 -0400

    added some ugly json data for houses,but we could still use a better data set at some point. added in the basic google map to the Search Results (or properties) route

--- app/scripts/controllers/properties_controller.js
+++ app/scripts/controllers/properties_controller.js
@@ -1,10 +1,8 @@
-function initialize() {
-  mapCanvas = $("#map-canvas")[0];
-  var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-  google.maps.event.addDomListener(window, 'load', initialize);
+App.PropertiesController = Ember.ArrayController.extend({
+
+  init: function(){ 
+    this._super();
+    console.log('init run');
+  }
+
+});
