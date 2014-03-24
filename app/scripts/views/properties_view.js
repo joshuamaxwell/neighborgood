1ed90e544a6a60c562a86c5ba66d85b5bc1d7a09
JMaxwell <maxwell.joshua@gmail.com>
2014-03-24 08:48:49 -0400

    i've got a marker rendered to the map finally

--- app/scripts/views/properties_view.js
+++ app/scripts/views/properties_view.js
@@ -1,7 +1,7 @@
 App.PropertiesView = Ember.View.extend({
 
-  didInsertElement: function (obj) {
-    this._super();
+  didInsertElement: function () {
+    // this._super(); //I don't think an event has to call this._super()
     console.log('enter the didInsertElement from the view');
 
     //figuring out where to call this .loadMap() function was
@@ -60,7 +60,32 @@ App.PropertiesView = Ember.View.extend({
   }
 ]
     };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    // I need this map variable to be accessable outside this function. I'd prefer to
+    //store it somewhere in the controller or find out some way
+    //to pass it to the controller.... I FOUND IT. I keep forgetting to always use the 
+    //'getters and setters' I have access to the controller's properties from 
+    //the view, i just have to use the this.get or this.set..
+    //so now I can use a property called 'map' from inside this controller
+    //....but looks like the best place to do any of this is still inside the view
+    // because I don't see any other way to be sure that the DOM has finished
+    // rendering
+    this.set('googleMap', new google.maps.Map(mapCanvas, mapOptions));
+    this.setMarkers();
+    //now i maybe need to run this function over and over for each item in
+    //the array by passing the properties in as arguments
+  },
+
+  setMarkers: function(){
+    //need to modify this function to accept arguments from the array
+    console.log('setMarkers fired');
+    var myLatlng = new google.maps.LatLng(34.842184, -82.395177);
+    
+    // To add the marker to the map, use the 'googleMap' property
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: this.get('googleMap'),
+        title:"Hello World!"
+    });
   }
 
 })
