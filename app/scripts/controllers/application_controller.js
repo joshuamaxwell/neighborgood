f95b69f1bb308276518fb5857602dccdccbdfc4a
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-03-26 17:01:55 -0400

    Gave nav bar background opacity on scroll. Added height of bar. Added home icon and hid filter icon.

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -1,9 +1,4 @@
 App.ApplicationController = Ember.ObjectController.extend({
 
-	// $('.navigation-bar').toggleClass(function(){
-	// 	if (this.scrollTop() >= 100){
-	// 		return 'foggy';
-	// 	}
-	// });
 
 })
