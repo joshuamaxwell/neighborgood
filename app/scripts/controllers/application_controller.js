d1dfab522aec561d93f5b68ded74616c3e77bd48
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-03-26 14:23:02 -0400

    Initial commit to branch

---
+++ app/scripts/controllers/application_controller.js
@@ -0,0 +1,10 @@
+App.ApplicationController = Ember.ObjectController.extend({
+
+
+	// $('.navigation-bar').toggleClass(function(){
+	// 	if (this.scrollTop() >= 100){
+	// 		return 'foggy';
+	// 	}
+	// });
+
+})
