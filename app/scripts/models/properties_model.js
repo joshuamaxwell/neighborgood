b696a8cc832456eb8fc10031acacedb4ef8e9166
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-03-30 14:26:25 -0400

    Added bedrooms and home picture to properties model and home detail page.

--- app/scripts/models/properties_model.js
+++ app/scripts/models/properties_model.js
@@ -3,6 +3,8 @@ var attr = DS.attr;
 App.Properties = DS.Model.extend({
   address: attr('string'),
   bathrooms: attr('number'),
+  bedrooms: attr('number'),
+  picture: attr('string'),
   description: attr('string'),
   property_id: attr('number'),
   price: attr('number'),
